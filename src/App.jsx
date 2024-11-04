7a64ff0b08786aa573f89346b8a76686aaed025d
TomisN17 <tomisquin@outlook.com>
2024-11-04 01:11:08 -0300

    segundo commit, header arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './assets/components/header';
 import Nav from './assets/components/Nav'
 import Hero from './assets/components/Hero';
 import About from './assets/components/About';
 import Topics from './assets/components/Topics';
 import Contact from './assets/components/Contact';
 import './App.css';
+import Header from './assets/components/header';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Header/>
       <Nav />
       <Hero />
       <About />
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -4,12 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Proyecto MESI</h1>
-      
     </header>
-
-
-
-    
   );
 };
 
